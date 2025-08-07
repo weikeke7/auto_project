60b05e1069561f849bede35c2c189abc7d291c1a
keke <1483035228@qq.com>
2025-08-07 22:32:05 +0800

    补全测试用例

--- src/test_pytest/plugin.py
+++ src/test_pytest/plugin.py
@@ -3,27 +3,38 @@ import pytest
 import requests
 
 data = {"failed":0,
-        "success": 0}
-
-
+        "success": 0,
+        "total":False}
+#读取配置文件
+def pytest_addoption(parser):
+    parser.addini(
+        "send_when",
+        help="什么时候发送企微信息"
+    )
+    parser.addini(
+        "send_address",
+        help="发送的地址"
+    )
 def pytest_collection_finish(session: pytest.Session) :
     #用例加载完成后执行，包含全部用例
-    data["total"] = len(session.items)
+    real_items = [i for i in session.items if i.nodeid.split("::")[-1] != "test_pass"]
+    data["total"] += len(real_items)
     print(f'用例的总数是{data["total"]}')
 
-def pytest_configure():
-    #测试开始前执行
+def pytest_configure(config : pytest.Config):
+    #配置加载之后,用例执行之前
     data["start_time"] = datetime.now()
-    print(f"{datetime.now()}pytest开始执行")
-
+    data["send_when"] = config.getini("send_when")
+    data["send_address"] = config.getini("send_address")
+    print(data["send_address"])
 
 def pytest_runtest_logreport(report : pytest.TestReport):
     #执行每条用例时
     if report.when == "call":
-        if report.outcome == "passed":
+        if report.outcome == "passed" and report.nodeid.split("::")[-1] != "test_pass":
             print(f"本次用例执行结果为{report.outcome}")
             data["success"] += 1
-        else:
+        elif report.nodeid.split("::")[-1] != "test_pass":
             print(f"本次用例执行结果为{report.outcome}")
             data["failed"] += 1
 
@@ -31,12 +42,13 @@ def pytest_unconfigure():
     #测试结束后执行
     data["end_time"] = datetime.now()
     print(f"{datetime.now()}pytest结束执行")
+
     data["duration"] = (data["end_time"] - data["start_time"]).total_seconds()
-
-
-
-    data["pass_retion"] = data["success"] / data["total"] * 100
-    data["pass_retion"] = f'{data["pass_retion"]:.2f}%'
+    data["pass_retion"] = (data["success"] / (data["failed"]+data["success"]) )* 100
+    if data["pass_retion"] >= 100:
+        data["pass_retion"] = 100
+    else:
+        data["pass_retion"] = f'{data["pass_retion"]:.2f}%'
 
 
 
@@ -44,39 +56,33 @@ def pytest_unconfigure():
     print(f'运行用例总数：{data["total"]}')
     print(f'用例成功数：{data["success"]}')
     print(f'用例失败数：{data["failed"]}')
-    print(f'用例成功率时长：{data["pass_retion"]}')
-
-    assert data["duration"]
-    assert data["total"] == 3
-    assert data["success"] == 2
-    assert data["failed"] == 1
-    assert data["pass_retion"] == "66.67%"
+    print(f'用例成功率：{data["pass_retion"]}%')
+    if data["send_when"] == "on_fail" and data["failed"] !=0 and data["send_address"] :
+        data["send_tag"] = True
+        send_result()
 
 
 
-    #发送测试结果到企业微信
-
-    url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9967d7b8-8a0d-412d-bded-61b74649bb85'
-
-    content =f"""
-    测试结果：
-    
-    测试时间：{ data["start_time"]}
-    用例数量： {data["total"] }
-    执行时长： {data["duration"]}s
-    测试通过数： {data["success"]}
-    测试失败数：<font color=\"warning\">{data["failed"]}</font>
-    测试通过率： {data["pass_retion"]}
-    
-    测试报告地址： """
-
-    json = {
-        "msgtype": "markdown",
-        "markdown": {
-            "content": content  },
-        "mentioned_list":["wangqing","@all"],
-    }
+def send_result():
 
 
+        url = data["send_address"]
+        content =f"""
+        测试结果：   
+        测试时间：{ data["start_time"]}
+        用例数量： {data["total"] }
+        执行时长： {data["duration"]}s
+        测试通过数： {data["success"]}
+        测试失败数：<font color=\"warning\">{data["failed"]}</font>
+        测试通过率： {data["pass_retion"]}
+        测试报告地址： """
 
-    requests.post(url,json=json)
+        json = {
+            "msgtype": "markdown",
+            "markdown": {
+                "content": content  },
+
+        }
+
+        requests.post(url,json=json)
+
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,11 +1,44 @@
-import time
+import pytest
+pytest_plugins = 'pytester'
+from test_pytest import plugin
 
-def test_api():
-    time.sleep(2.5)
 
-def test_api1():
-    time.sleep(2.5)
+@pytest.mark.parametrize("send_when",["every","on_fail"])
+def test_send_when(send_when,pytester : pytest.Pytester,tmp_path):#tmp_path 用于生成保存一个临时的配置文件
+    config_path = tmp_path.joinpath("pytest.ini")
+    config_path.write_text(
+        f"[pytest]\n"
+        f"send_when = {send_when}\n"
+         "send_address = https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9967d7b8-8a0d-412d-bded-61b74649bb85\n"
+    )
+    config = pytester.parseconfig(config_path)
+    assert  config.getini("send_when") == send_when
 
-def test_api2():
-    time.sleep(2.5)
-    raise Exception
+    pytester.makepyfile("""
+    def test_pass():
+        ...
+        """)
+    pytester.runpytest("-c",str(config_path),"-s")
+    if send_when == "every":
+        raise Exception
+
+
+
+@pytest.mark.parametrize("send_address",["","https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=9967d7b8-8a0d-412d-bded-61b74649bb85"])
+def test_send_address(send_address,pytester : pytest.Pytester,tmp_path):
+    config_path = tmp_path.joinpath("pytest.ini")
+    config_path.write_text(
+        f"[pytest]\n"
+        f"send_address = {send_address}\n"
+        "send_when = every")
+    config = pytester.parseconfig(config_path)
+    assert config.getini("send_address") == send_address
+    pytester.makepyfile("""
+     def test_pass():
+         ...
+         """)
+    pytester.runpytest("-c", str(config_path), "-s")
+
+
+
+
